1ca0b3c6aca07cd57ac0c41693a82acb12f790ac
YukJiSoo <wltn3231@naver.com>
2019-09-12 23:35:44 +0900

    [Update] email Validator 중복검사 추가

--- constants.js
+++ constants.js
@@ -7,6 +7,7 @@ function define(name, value) {
 
 define('ERR_MESSAGE', {
     EMAIL_FORMAT: '잘못된 email 형식입니다',
+    EMAIL_DUPLICATION: '중복된 email입니다',
     PASSWORD_LENGTH: '8자 이상 16자 이하로 입력해주세요',
     PASSWORD_ALPHA: '영문을 최소 1자이상 포함해주세요',
     PASSWORD_NUMBER: '숫자를 최소 1자 이상 포함해주세요',
--- util/validators/email.js
+++ util/validators/email.js
@@ -1,7 +1,20 @@
 const { ERR_MESSAGE } = require('../../constants');
+const DBManager = require('../../service/db-manager');
 
-module.exports = (email) => {
+module.exports = async (email) => {
     const regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    return regex.exec(email) !== null
-        ? { success: true } : { success: false, message: ERR_MESSAGE.EMAIL_FORMAT };
+    if (regex.exec(email) === null) {
+        const result = { success: false, message: ERR_MESSAGE.EMAIL_FORMAT };
+        return new Promise((reject) => reject(result));
+    }
+
+    const whereClause = { field: 'email', operator: '==', value: email };
+    const snapshot = await DBManager.read({ collection: 'account' }, [whereClause]);
+
+    if (!snapshot.empty) {
+        const result = { success: false, message: ERR_MESSAGE.EMAIL_DUPLICATION };
+        return new Promise((resolve) => resolve(result));
+    }
+
+    return new Promise((resolve) => resolve({ success: true }));
 };
